0c0372bb64a99f0310322a7f767bc0963022e11e
Futa Ogawa <ogawa0071@gmail.com>
2024-03-08 23:04:41 +0900

    Add TiDB and Prisma.

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,22 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// https://docs.pingcap.com/tidbcloud/serverless-driver-prisma-example
+// https://prismaio.notion.site/Prisma-Edge-Functions-Early-Access-062612545bbd436d9705b43f65655669
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+import { connect } from "@tidbcloud/serverless";
+import { PrismaTiDBCloud } from "@tidbcloud/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+interface Env {
+  DATABASE_URL: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const connection = connect({ url: env.DATABASE_URL });
+    const adapter = new PrismaTiDBCloud(connection);
+    const prisma = new PrismaClient({ adapter });
+
+    const users = await prisma.user.findMany();
+    const result = JSON.stringify(users);
+    return new Response(result);
+  },
 };
